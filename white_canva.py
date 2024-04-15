7373bdcafd3e1ae2e86fb22f1df99a72a5e3e989
Maria Soler <juliasolernunes@ymail.com>
2024-04-15 12:20:48 +0200

    This is the button that toogles the color

--- white_canva.py
+++ white_canva.py
@@ -8,9 +8,22 @@ class DrawingApp:
         print('This is the testing')
         self.canvas.bind("<B1-Motion>", self.draw)
 
+          # Add a button to change color
+        self.color_button = tk.Button(root, text="Change Color", command=self.change_color)
+        self.color_button.pack()
+
+        # Default color
+        self.color = "black"
     def draw(self, event):
         x, y = event.x, event.y
-        self.canvas.create_oval(x, y, x+5, y+5, fill="black")
+        self.canvas.create_oval(x, y, x+5, y+5, fill=self.color)
+
+    def change_color(self):
+        # Toggle between black and red colors
+        if self.color == "black":
+            self.color = "red"
+        else:
+            self.color = "black"
 
 if __name__ == "__main__":
     root = tk.Tk()
