d9301c4d75a244df03f5a191b10155350b33216d
Maria Soler <juliasolernunes@ymail.com>
2024-04-15 12:38:52 +0200

    Code changes were merged

--- white_canva.py
+++ white_canva.py
@@ -10,35 +10,19 @@ class DrawingApp:
         # Add a button to change color
         self.color_button = tk.Button(root, text="Change Color", command=self.change_color)
         self.color_button.pack()
-
+        # I am adding a comment
         self.color = "black"
 
-          # Add a button to change color
-        self.color_button = tk.Button(root, text="Change Color", command=self.change_color)
-        self.color_button.pack()
-
-        # Default color
-        self.color = "black"
     def draw(self, event):
         x, y = event.x, event.y
         self.canvas.create_oval(x, y, x+5, y+5, fill=self.color)
 
-<<<<<<< HEAD
-    def change_color(self):
-        # Toggle between black and red colors
-        if self.color == "black":
-            self.color = "red"
-        else:
-            self.color = "black"
-=======
 
     def change_color(self):
         # Open a color chooser dialog to select a color
         color = colorchooser.askcolor(title="Choose a color")[1]  # askcolor returns a tuple, we need the second element
         if color:  # If a color was selected
             self.color = color  # Update the current color
-
->>>>>>> 1c3b430fbd38f3ac5190dd42ac58bde85dd86a49
 
 if __name__ == "__main__":
     root = tk.Tk()
