1c3b430fbd38f3ac5190dd42ac58bde85dd86a49
unknown <alon.gal@masterschool.com>
2024-04-15 13:04:53 +0300

    Added a new color picker, I rock!

--- white_canva.py
+++ white_canva.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import colorchooser  # Import the colorchooser module
 class DrawingApp:
     def __init__(self, root):
         self.root = root
@@ -7,10 +7,23 @@ class DrawingApp:
         self.canvas.pack()
         print('This is the testing')
         self.canvas.bind("<B1-Motion>", self.draw)
+        # Add a button to change color
+        self.color_button = tk.Button(root, text="Change Color", command=self.change_color)
+        self.color_button.pack()
+
+        self.color = "black"
 
     def draw(self, event):
         x, y = event.x, event.y
-        self.canvas.create_oval(x, y, x+5, y+5, fill="black")
+        self.canvas.create_oval(x, y, x+5, y+5, fill=self.color)
+
+
+    def change_color(self):
+        # Open a color chooser dialog to select a color
+        color = colorchooser.askcolor(title="Choose a color")[1]  # askcolor returns a tuple, we need the second element
+        if color:  # If a color was selected
+            self.color = color  # Update the current color
+
 
 if __name__ == "__main__":
     root = tk.Tk()
